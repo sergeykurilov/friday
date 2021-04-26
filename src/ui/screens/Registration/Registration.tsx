939962698524a0ccf2e80dbaea276de36506d73e
zhankaam <zhankaam@gmail.com>
2021-04-26 22:53:15 +0300

    registration page

--- src/ui/screens/Registration/Registration.tsx
+++ src/ui/screens/Registration/Registration.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {PATH} from "../../components/Nav/Navigation";
+import {useSelector} from "react-redux";
 
 
 export default function Registration() {
+
+   // const loading = useSelector()
+
+
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -74,8 +81,9 @@ export default function Registration() {
                                 type="submit"
                                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
-                                Sign in
+                                Sign up
                             </button>
+
                         </div>
                     </form>
 
