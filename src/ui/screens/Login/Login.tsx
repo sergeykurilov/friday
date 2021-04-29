4a7b79059d21d160c6c42d7276770c252bd20605
zhankaam <zhankaam@gmail.com>
2021-04-29 16:11:24 +0300

    registration page

--- src/ui/screens/Login/Login.tsx
+++ src/ui/screens/Login/Login.tsx
@@ -5,13 +5,13 @@ import {Redirect} from "react-router-dom";
 import {loginTC} from "../../redux/thunk/login/loginTh";
 
 export function Login(props: any) {
-    console.log(props)
+
     const isAuth = useSelector((state:any) => state.authorization.isAuth)
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [rememberMe, setRememberMe] = useState(false)
     const dispatch = useDispatch()
-    console.log(isAuth)
+
     if(props.isAuth === true){
         return <Redirect to="/home"/>
     }
@@ -20,15 +20,17 @@ export function Login(props: any) {
         return <Redirect to="/home"/>
     }
 
+    const onClickHandler = () => dispatch(loginTC(resData));
+
     return (
         <div>
             <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                    <img
-                        className="mx-auto h-12 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
-                    />
+                    {/*<img*/}
+                    {/*    className="mx-auto h-12 w-auto"*/}
+                    {/*    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"*/}
+                    {/*    alt="Workflow"*/}
+                    {/*/>*/}
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
 
                 </div>
@@ -97,7 +99,7 @@ export function Login(props: any) {
                             </div>
                             <div>
                                 <button
-                                    onClick={() => dispatch(loginTC(resData))}
+                                    onClick={onClickHandler}
                                     type="submit"
                                     className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 >
@@ -119,7 +121,7 @@ export function Login(props: any) {
                             <div className="mt-6 grid grid-cols-3 gap-3">
                                 <div>
                                     <a
-                                        href="#"
+                                        href="https://www.facebook.com"
                                         className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                                     >
                                         <span className="sr-only">Sign in with Facebook</span>
@@ -135,7 +137,7 @@ export function Login(props: any) {
 
                                 <div>
                                     <a
-                                        href="#"
+                                        href="https://twitter.com/"
                                         className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                                     >
                                         <span className="sr-only">Sign in with Twitter</span>
@@ -148,7 +150,7 @@ export function Login(props: any) {
 
                                 <div>
                                     <a
-                                        href="#"
+                                        href="https://github.com/"
                                         className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                                     >
                                         <span className="sr-only">Sign in with GitHub</span>
