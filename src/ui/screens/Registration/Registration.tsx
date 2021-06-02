d1aaa8c2fc4b11be349a1e7ffc1d09cebcae2723
zhankaam <zhankaam@gmail.com>
2021-06-02 23:42:54 +0300

    search form in Packs.tsx from Zhanat :)

--- src/ui/screens/Registration/Registration.tsx
+++ src/ui/screens/Registration/Registration.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {RootStateType} from "../../redux/store/store";
 import {RequestStatusType} from "../../redux/reducers/registration/registration-reducer";
@@ -20,7 +20,7 @@ export default function Registration() {
     const isRegistration = useSelector<RootStateType, boolean>(registration => registration.registration.isRegistration)
 
 
-    const status = useSelector<RootStateType, RequestStatusType>(registration => registration.registration.status)
+    // const status = useSelector<RootStateType, RequestStatusType>(registration => registration.registration.status)
     const dispatch = useDispatch();
 
 
@@ -40,12 +40,18 @@ export default function Registration() {
             </div>
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <ValidateInput funcName={"Email address"} value={email}
-                                   onChange={handleEmailChange(setEmail, setEmailError)} required={true}
-                                   type={"email"} name={"email"} id={"email"} error={emailError}/>
-                    <ValidateInput funcName={"Password"} value={password}
-                                   onChange={handlePasswordChange(setPassword, setPasswordError)} required={true}
-                                   type="password" name="password" id="password" error={passwordError}/>
+                    <div>
+                        <div className="mt-1">
+                            <ValidateInput funcName={"Email address"} value={email}
+                                           onChange={handleEmailChange(setEmail, setEmailError)} required={true}
+                                           type={"email"} name={"email"} id={"email"} error={emailError}/>
+                        </div>
+                    </div>
+                    <div className="mt-1">
+                        <ValidateInput funcName={"Password"} value={password}
+                                       onChange={handlePasswordChange(setPassword, setPasswordError)} required={true}
+                                       type="password" name="password" id="password" error={passwordError}/>
+                    </div>
                     <div className="flex items-center justify-between">
                         <div className="flex items-center">
                             <input
@@ -59,31 +65,29 @@ export default function Registration() {
                             <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
                                 Remember me
                             </label>
-                        </div>
 
-                        <div className="text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            <a href={PATH.FORGOT} className="font-medium text-indigo-600 hover:text-indigo-500">
                                 Forgot your password?
                             </a>
                         </div>
-
                     </div>
 
                     <div>
-                        <button
-                            type="submit"
-                            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            // disabled={status === "loading"}
-                            onClick={onRegistrationCallback}
-                        >
-                            Sign up
-                        </button>
+                        <div>
+                            <button
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                // disabled={status === "loading"}
+                                onClick={onRegistrationCallback}
+                            >
+                                Sign up
+                            </button>
 
+                        </div>
                     </div>
+                    <WithSocial/>
                 </div>
 
-
-                <WithSocial/>
 
             </div>
         </div>
